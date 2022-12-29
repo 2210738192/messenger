b099d92e5da3909a35baf6bdc865e8d8fdca5425
David Specht <2210738192@stud.fh-kufstein.ac.at>
2022-12-29 11:02:29 +0100

    uii

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 "use strict";
 exports.__esModule = true;
 var ws_1 = require("ws");
-var wss = new ws_1.WebSocketServer({ port: 8080 });
+// import express from "express";
+var express = require("express");
+var app = express();
+var path = require('path');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+    console.log("index requested");
+});
+app.get("/:dir", function (req, res) {
+    res.sendFile(__dirname + "/" + req.params.dir);
+    console.log("dir " + req.params.dir + " requested");
+});
+app.listen(443);
+var wss = new ws_1.WebSocketServer({ port: 5000 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(data, isBinary) {
         wss.clients.forEach(function each(client) {
             if (client.readyState === ws_1.WebSocket.OPEN) {
-                let message = JSON.parse(data.toString());
-                message.time = new Date();
-                
                 // let message = {
                 //   "data": data,
                 //   "time": new Date(),
                 //   "user": data.user
                 // }
-                client.send(JSON.stringify(message), { binary: isBinary });
+                client.send(data, { binary: isBinary });
             }
         });
     });
--- app.ts
+++ app.ts
@@ -1,6 +1,23 @@
 import {WebSocket, WebSocketServer} from 'ws';
+// import express from "express";
+import express = require("express");
+import {readFile} from 'fs/promises';
 
-const wss = new WebSocketServer({ port: 8080 });
+
+const app = express();
+const path = require('path');
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+    console.log("index requested");
+})
+app.get("/:dir", (req, res) => {
+  res.sendFile(__dirname + "/" + req.params.dir);
+  console.log("dir " + req.params.dir + " requested");
+})
+app.listen(443);
+
+const wss = new WebSocketServer({ port: 5000 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data, isBinary) {
